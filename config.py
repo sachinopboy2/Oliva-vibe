6d74ed0aec9e52603646c933bebd0f5fc81ff417
sachinopboy2 <sachin25u7@gmail.com>
2025-09-15 20:24:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/nandrepo1/Sanatani-vibe",
+    "https://github.com/sachinopboy2/Oliva-vibe",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
